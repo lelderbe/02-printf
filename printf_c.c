58229a01352e49651b587649b9064d59e18e4966
lelderbe <lelderbe@student.21-school.ru>
2020-12-30 15:21:28 +0300

    %c done but 0 byte

--- printf_c.c
+++ printf_c.c
@@ -6,43 +6,58 @@
 /*   By: lelderbe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/29 14:13:04 by lelderbe          #+#    #+#             */
-/*   Updated: 2020/12/29 15:36:57 by lelderbe         ###   ########.fr       */
+/*   Updated: 2020/12/30 15:19:15 by lelderbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static void	fill_width(char *result, int size, t_spec *spec)
+static void	fill_width(char *result, int size, t_spec *e)
 {
 	ft_memset(result, ' ', size);
-	if (spec->flags.zero && spec->precision < 0)
+	if (e->flags.zero && e->precision < 0)
 		ft_memset(result, '0', size);
 }
 
-static void	fill_precision(char *result, int size, t_spec *spec)
+static void	fill_precision(char *result, int size, t_spec *e)
 {
 	(void)result;
 	(void)size;
-	(void)spec;
+	(void)e;
 }
 
-char	*get_c_result(t_spec *spec)
+static void	fill_data(char *result, int size, t_spec *e)
+{
+	int		pos;
+
+	if (e->flags.left)
+		pos = 0;
+	else
+		pos = size - 1;
+	result[pos] = e->value.c;
+}
+
+static int	get_size(t_spec *e)
+{
+	int		size;
+
+	(void)e;
+	size = e->width ? e->width : 1;
+	return (size);
+}
+
+char		*get_c_result(t_spec *e)
 {
 	char	*result;
 	size_t	size;
-	int		i;
 
-	size = spec->width ? spec->width : 1;
+	size = get_size(e);
 	result = malloc(sizeof(*result) * (size + 1));
 	if (!result)
 		return (0);
-	fill_width(result, size, spec);
-	fill_precision(result, size, spec);
-	if (spec->flags.left)
-		i = 0;
-	else
-		i = size - 1;
-	result[i] = spec->value.c;
+	fill_width(result, size, e);
+	fill_precision(result, size, e);
+	fill_data(result, size, e);
 	result[size] = '\0';
 	return (result);
 }
